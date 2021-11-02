4872f7b4b540c996df719febe0d8ac8736a94e80
Ginver <74971813+Ginver@users.noreply.github.com>
2021-11-02 15:21:26 +0100

    dashboard component created

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import  Login  from './Login';
 import useLocalStorage from "../hooks/useLocalStorage";
+import Dashboard from "./Dashboard";
 
 function App() {
-    // const [ id, setId ] = useState()
+    // const [ id, setId ] = useState() - useState will be exchanged by useLocalStorage
     const [ id, setId ] = useLocalStorage('id')
 
   return (
-      <>
-          {id}
-          <Login onIdSubmit={setId} />
-      </>
+      // if you have an id you want to go to another page,
+      //   if not you want to go to this login page and otherwise
+      //   you want to go to the Dashboard:
+          id ? <Dashboard id={id}/> : <Login onIdSubmit={setId}/>
   );
 }
 
